4369f9ad6f9027b2bac1d111971975877ee038a9
Dylan Sigler <jsigler47@gmail.com>
2023-10-18 13:44:57 -0500

    Write data to file instead of printing to console

--- rssmon.py
+++ rssmon.py
@@ -1,25 +1,36 @@
 import feedparser
 import sys
 import validators
+import os
+from datetime import datetime
 
 def fetch_rss_feed(url):
-    # Validate the URL
     if not validators.url(url):
         print("Invalid URL provided. Please provide a valid RSS feed URL.")
         sys.exit(1)
 
     feed = feedparser.parse(url)
 
-    print(f"Title: {feed.feed.title}")
-    print(f"Number of Entries: {len(feed.entries)}")
-    print("-" * 50)
+    output_directory = "output"
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_filename = f"{output_directory}/{current_time}.txt"
 
-    for entry in feed.entries:
-        print(f"Title: {entry.title}")
-        print(f"Link: {entry.link}")
-        print(f"Published: {entry.published}")
-        print(f"Summary: {entry.summary}")
-        print("-" * 50)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    with open(output_filename, 'w') as file:
+        file.write(f"Title: {feed.feed.title}\n")
+        file.write(f"Number of Entries: {len(feed.entries)}\n")
+        file.write("-" * 50 + "\n")
+
+        for entry in feed.entries:
+            file.write(f"Title: {entry.title}\n")
+            file.write(f"Link: {entry.link}\n")
+            file.write(f"Published: {entry.published}\n")
+            file.write(f"Summary: {entry.summary}\n")
+            file.write("-" * 50 + "\n")
+
+    print(f"Feed data written to: {output_filename}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
